869376eeebceb9b4df029fdc7209e989d106f952
Anderson <andersonv12@gmail.com>
2023-09-21 14:46:07 -0300

    Adicionada a funcionalidade de somar

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ print("Calculadora em Python")
 numero1 = int (input("Informe um número inteiro"))
 numero2 = int (input("Informe outro número inteiro"))
 operacao = input("Informe a operacao (+ - * /)")
+
+if operacao == "+":
+	print("Resultado:", numero1 + numero2)
